cd03020ec5889331d667e69802e33d20f49f4cb0
Do Trung Kien <trungkien.keio@gmail.com>
2020-11-20 09:56:00 +0700

    update project

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-require('dotenv').config;
-
 const AUTH_SERVER = 'https://auth.oasiscloud.io';
 
 const config = {
@@ -10,8 +8,8 @@ const config = {
     jwks_uri: AUTH_SERVER + '/oauth/keys',
     token_endpoint: AUTH_SERVER + '/oauth/token',
   },
-  client_id: process.env.OASIS_CLIENT_ID,
-  redirect_uri: 'http://localhost:4050/callback',
+  client_id: '40bf6246-cfc7-48e0-b8f1-3a0d3462e17d',
+  redirect_uri: 'http://localhost:5000/callback',
   response_type: 'code',
   scope: 'openid',
   filterProtocolClaims: false,
