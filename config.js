ded351e8030be7ca80371c693da542c1729938f4
Do Trung Kien <trungkien.keio@gmail.com>
2020-11-20 07:55:26 +0700

    try retrieving user identities

---
+++ config.js
@@ -0,0 +1,22 @@
+require('dotenv').config;
+
+const AUTH_SERVER = 'https://auth.oasiscloud.io';
+
+const config = {
+  authority: AUTH_SERVER,
+  metadata: {
+    issuer: AUTH_SERVER,
+    authorization_endpoint: AUTH_SERVER + '/oauth/authorize',
+    jwks_uri: AUTH_SERVER + '/oauth/keys',
+    token_endpoint: AUTH_SERVER + '/oauth/token',
+  },
+  client_id: process.env.OASIS_CLIENT_ID,
+  redirect_uri: 'http://localhost:4050/callback',
+  response_type: 'code',
+  scope: 'openid',
+  filterProtocolClaims: false,
+  loadUserInfo: false,
+  extraQueryParams: {
+    audience: 'https://api.oasislabs.com/parcel',
+  },
+};
