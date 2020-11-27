5aab1d10015303ffa4262a9dd490dfdea038bd69
Do Trung Kien <trungkien.keio@gmail.com>
2020-11-27 15:24:15 +0700

    update get datasets

--- server/routes/files.js
+++ server/routes/files.js
@@ -6,20 +6,20 @@ const { getAllFiles } = require('./helpers');
 router.get('/', async (req, res) => {
   const { address, token } = req.headers;
 
-  const datasets = await fetch('https://parcel-sdk.oasiscloud.io/api/state/identity/getDatasets', {
-    headers: {
-      authorization: `Bearer ${token}`,
-      'content-type': 'application/json',
-    },
-    body: { identity: address, owned: false },
-    method: 'POST',
-    // mode: 'cors',
-  });
+  // const datasets = await fetch('https://parcel-sdk.oasiscloud.io/api/state/identity/getDatasets', {
+  //   headers: {
+  //     authorization: `Bearer ${token}`,
+  //     'content-type': 'application/json',
+  //   },
+  //   body: { identity: address, owned: false },
+  //   method: 'POST',
+  //   // mode: 'cors',
+  // });
 
-  console.log({ datasets });
-  // const allDatasets = await getAllFiles(address, token);
-  // if (!!allDatasets) res.status(200).send(allDatasets);
-  // else res.status(500).send({ msg: 'no dataset found' });
+  // console.log({ datasets });
+  const allDatasets = await getAllFiles(address, token);
+  if (!!allDatasets) res.status(200).send(allDatasets);
+  else res.status(500).send({ msg: 'no dataset found' });
 });
 
 module.exports = router;
